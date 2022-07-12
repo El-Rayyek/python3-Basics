5bc8900df8026f0dbc08cd4a72d1e8029ffad8e5
unknown <mohamed.abdelfattah439@eng.s-mu.edu.eg>
2022-07-13 01:15:42 +0200

    add python conditions

---
+++ 01-conditions.py
@@ -0,0 +1,82 @@
+#simple condition
+'''
+x = 5
+if x == 3:
+    print("x = 3")
+else:
+    print(x)
+'''
+
+# if elif else
+'''
+x = 10
+
+if x == 3:
+    print("x = 3")
+
+elif x < 2:
+    print("x > 2")
+
+elif x < 6 :
+    print("x < 6")
+
+
+else:
+    print(x)
+'''
+
+
+# nested if
+'''
+x = 6
+
+if x < 10 :
+    print("x < 10")
+    if x < 5 :
+        print("x < 5")
+    else :
+        print("x > 5")
+
+else:
+    print("x > 10")
+'''
+
+### if with boolean operators
+
+user_name = 'mohamed'
+password  = 12345
+
+if user_name == 'mohamed' and password == 12345 :
+    print('welcome and')
+
+if user_name == 'mohamed' or password == 1234567 :
+    print('welcome or')
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
